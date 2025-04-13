7748f73233ff8b8a11924ce0154d3e5bfa2ad577
wai-lin <dev.wailin@gmail.com>
2025-04-13 23:52:59 +0700

    feat(UText): p tag component

---
+++ components/U/Text.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+type Props = {
+   as?: string;
+   variant?: "default" | "subtle";
+};
+const { as = "p", variant = "default" } = defineProps<Props>();
+
+const classes = tv({
+   variants: {
+      variant: {
+         default: "text-base",
+         subtle: "text-sm",
+      },
+   },
+});
+</script>
+
+<template>
+   <component :is="as" :class="classes({ variant })" v-tw-merge>
+      <slot />
+   </component>
+</template>
