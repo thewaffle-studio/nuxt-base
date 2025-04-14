7586c5b360f1f3e5053e248ee98ceeccf5a4754a
wai-lin <dev.wailin@gmail.com>
2025-04-14 10:01:38 +0700

    fix(nuxt.config.ts): resolve absolute paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,22 @@
 import tailwindcss from "@tailwindcss/vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+// To resolve correct path when being used as a layer
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const resolvePath = (path: string) => resolve(currentDir, path);
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
    compatibilityDate: "2024-11-01",
    devtools: { enabled: true },
+   $meta: {
+      name: "nuxt-base", // layer name
+   },
    vite: {
       plugins: [tailwindcss()],
    },
    modules: ["@nuxt/ui", "@nuxt/image"],
-   plugins: ["./plugins/directives/tailwind-merge.ts"],
-   css: ["./assets/css/main.css"],
+   plugins: [resolvePath("./plugins/directives/tailwind-merge.ts")],
+   css: [resolvePath("./assets/css/main.css")],
 });
